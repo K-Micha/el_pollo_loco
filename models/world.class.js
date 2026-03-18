68f03553d12d727a0ef32c1dd70e2ac40ab25268
K-Micha <Micha.kuehn91@gmail.com>
2026-03-18 14:19:14 +0100

    added movable

---
+++ models/world.class.js
@@ -0,0 +1,67 @@
+class World {
+    character = new Character();
+
+    enemyTypes = [Chicken, SmallChicken];
+
+    enemies = [
+        this.randomEnemy(),
+        this.randomEnemy(),
+        this.randomEnemy(),
+        this.randomEnemy(),
+        this.randomEnemy(),
+        this.randomEnemy(),
+        this.randomEnemy(),
+        this.randomEnemy(),
+        this.randomEnemy(),
+    ];
+
+    clouds = [
+        new Cloud(),
+    ];
+    backgroundObjects = [
+        new BackgroundObject('assets/img/5_background/layers/air.png', 0),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 0)
+    ];
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
+
+    randomEnemy() {
+        let types = [Chicken, SmallChicken];
+        let Type = types[Math.floor(Math.random() * types.length)];
+        return new Type();
+    }
+
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.clouds);
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.enemies);
+
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+
+        });
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    }
+}
