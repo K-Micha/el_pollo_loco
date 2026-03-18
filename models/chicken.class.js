bb85a0a225c2f65d1afe70b4268e0320be1c7429
K-Micha <Micha.kuehn91@gmail.com>
2026-03-18 14:18:58 +0100

    added chicken

---
+++ models/chicken.class.js
@@ -0,0 +1,42 @@
+class Chicken extends MovableObject {
+
+    y = 360;
+    height = 60;
+    width = 80;
+
+    IMAGES_WALKING = [
+        'assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ];
+
+    constructor() {
+        super().loadImage('assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.x = 200 + Math.random() * 1200;
+        this.speed = 0.15 + Math.random() * 0.4;
+        this.animate();
+    }
+
+    animate() {
+        this.moveLeft();
+
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
+
+    }
+}
+
+/**
+  autoMove = setInterval(() => {
+    world.enemies.forEach(e => e.x -= 10);
+}, 100);
+
+ */
+
+//last keyboard objekt
