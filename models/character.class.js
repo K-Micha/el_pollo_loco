f319b60d87899be5a27b179d1965e2bf02570456
K-Micha <Micha.kuehn91@gmail.com>
2026-03-18 20:25:22 +0100

    update Character animate

--- models/character.class.js
+++ models/character.class.js
@@ -24,23 +24,25 @@ class Character extends MovableObject {
     animate() {
 
         setInterval(() => {
-                        if(this.world.keyboard.RIGHT){
+            if (this.world.keyboard.RIGHT) {
                 this.x += this.speed;
-                }
-                                        if(this.world.keyboard.LEFT){
+                this.otherDirection = false;
+            }
+            if (this.world.keyboard.LEFT) {
                 this.x -= this.speed;
-                }
+                this.otherDirection = true;
+            }
         }, 1000 / 60);
 
         setInterval(() => {
 
-            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT){
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
 
-            let i = this.currentImage % this.IMAGES_WALKING.length;
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
-             }
+                let i = this.currentImage % this.IMAGES_WALKING.length;
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
         }, 100);
 
     }
