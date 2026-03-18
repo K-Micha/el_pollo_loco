a67ef24fdb44fb0f3b1494e7ba3e4f1dcdb650f4
K-Micha <Micha.kuehn91@gmail.com>
2026-03-18 19:44:40 +0100

    added Animate Character

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
 
     height = 280;
     y = 155;
+    speed = 10;
     IMAGES_WALKING = [
         'assets/img/2_character_pepe/2_walk/W-21.png',
         'assets/img/2_character_pepe/2_walk/W-22.png',
@@ -10,6 +11,7 @@ class Character extends MovableObject {
         'assets/img/2_character_pepe/2_walk/W-25.png',
         'assets/img/2_character_pepe/2_walk/W-26.png',
     ];
+    world;
 
 
     constructor() {
@@ -20,11 +22,25 @@ class Character extends MovableObject {
     }
 
     animate() {
+
         setInterval(() => {
+                        if(this.world.keyboard.RIGHT){
+                this.x += this.speed;
+                }
+                                        if(this.world.keyboard.LEFT){
+                this.x -= this.speed;
+                }
+        }, 1000 / 60);
+
+        setInterval(() => {
+
+            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT){
+
             let i = this.currentImage % this.IMAGES_WALKING.length;
             let path = this.IMAGES_WALKING[i];
             this.img = this.imageCache[path];
             this.currentImage++;
+             }
         }, 100);
 
     }
