585fa80c028d43e585bf9f1f860cb270a22f623f
K-Micha <Micha.kuehn91@gmail.com>
2026-03-20 18:18:00 +0100

    update chicken

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -7,7 +7,22 @@ class MovableObject {
     imageCache = {};
     currentImage = 0;
     speed = 0.15;
+    speedY = 0;
+    acceleration = 2.5;
     otherDirection = false;
+
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);
+    }
+
+    isAboveGround() {
+        return this.y < 155;
+    }
 
     loadImage(path) {
         this.img = new Image();
@@ -23,13 +38,12 @@ class MovableObject {
     }
 
     moveRight() {
-
+        this.x += this.speed;
     }
 
     moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000 / 60);
+                        this.x -= this.speed;
+
     }
 
     playAnimation(images) {
@@ -37,5 +51,9 @@ class MovableObject {
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
+    }
+
+    jump() {
+        this.speedY = 30;
     }
 }
